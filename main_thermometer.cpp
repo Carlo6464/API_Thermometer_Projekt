8768ce2d589ac8eec5bee17731fd7116297c8809
Carl W.D <c.wagner-douglas@tu-braunschweig.de>
2017-04-18 17:37:39 +0200

    Main File
    Hauptdatei für das Programmieren unseres Codes

---
+++ main_thermometer.cpp
@@ -0,0 +1,9 @@
+//-------------------------------------------------------------------------------------------------
+
+// TU Braunschweig API Programmierprojekt SS 2017
+// Gruppenmitglieder: 	Carl Wagner-Douglas, Sina Gabor, Niklas Günther, Fabian ___, Fatih Mustafa
+// Filename: 			main_thermometer.cpp
+// created:	 			18-04-2017
+// last edited:			18-04-2017
+
+// ------------------------------------------------------------------------------------------------
